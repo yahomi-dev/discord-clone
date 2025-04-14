4b7aa58b7692e42a8170befb16d4f9ec50c723ae
yahomi-dev <yahomi.dev@gmail.com>
2025-04-14 21:35:11 +0900

    アイコン中央寄せ

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,12 +2,18 @@ import reactLogo from '../assets/react.svg';
 
 const SideBar = () => {
 	return (
-		<div className="sidebar flex flex-1 bg-gray-500">
-			<div className="sidebar-left">
-				<div>
-					<img src={reactLogo} className="logo react" alt="React logo" />
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</div>
+		<div className="sidebar flex flex-[0.35] h-[100vh] bg-gray-500">
+			<div className="sidebar-left flex flex-col mt-4 px-4 py-2 space-y-3 items-center bg-black">
+				{Array.from({ length: 2 }, (_, i) => {
+					return (
+						<div
+							key={`icon-${i}`}
+							className="server-icon flex items-center justify-center w-13 h-13  rounded-full bg-gray-700"
+						>
+							<img src={reactLogo} className="logo react" alt="React logo" />
+						</div>
+					);
+				})}
 			</div>
 			<div className="sidebar-right">SideBarRight</div>
 		</div>
